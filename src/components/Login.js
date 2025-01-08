51ef167f727c4611801372a51f7625c95cad752e
17sonu <97702823+17sonu@users.noreply.github.com>
2025-01-08 13:46:17 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = ({ setIsAuthenticated }) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://final-proposal-order-backend.vercel.app/api/auth/login",
         {
           username,
           password,
