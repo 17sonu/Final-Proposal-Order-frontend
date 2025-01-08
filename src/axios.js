add32b1e3fda51527ddb775854c1ab3e1d47eef6
17sonu <97702823+17sonu@users.noreply.github.com>
2025-01-08 13:41:55 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://final-proposal-order-backend.vercel.app/api/", // Change this if you deploy it
-  withCredentials: true, // Allow sending cookies
-});
-
-export default instance;
